e3d8ee557ff39422790cbb118417a675e03466cc
Golpette <stevenjamescourt@gmail.com>
2017-09-08 01:43:02 +0100

    Add extra bug to list in Sudoku.java

--- app/src/main/java/com/puzzle/andrew/sudowordsandroid/sudoku/Sudoku.java
+++ app/src/main/java/com/puzzle/andrew/sudowordsandroid/sudoku/Sudoku.java
@@ -20,7 +20,8 @@ import java.util.Random;
 // (1) The hint will not be correct if there is an incorrect entry; fix the method / give warning?
 // (2) Landscape not supported; deal with this
 // (3) If grid is full, crashes if Hint is pressed -- maybe just do congrats message and close?
-// (4)
+// (4) To get rid of hint highlighting you have to hit "Check" twice...
+
 
 
 /**
