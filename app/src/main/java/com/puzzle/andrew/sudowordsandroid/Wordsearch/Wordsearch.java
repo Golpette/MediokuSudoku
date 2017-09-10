a19a0b6167d7094d4a8965b8f536187f82c72a1d
AndrewRigg <AndrewJRigg@gmail.com>
2017-09-10 23:09:13 +0100

    Added import

--- app/src/main/java/com/puzzle/andrew/sudowordsandroid/Wordsearch/Wordsearch.java
+++ app/src/main/java/com/puzzle/andrew/sudowordsandroid/Wordsearch/Wordsearch.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.puzzle.andrew.sudowordsandroid.R;
 
