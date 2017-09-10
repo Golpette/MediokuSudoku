64db9cc3249b0a78aa91b888f38812512e75ab3b
AndrewRigg <AndrewJRigg@gmail.com>
2017-09-10 03:31:32 +0100

    Improved wordsearch UI

--- app/src/main/java/com/puzzle/andrew/sudowordsandroid/Wordsearch/Wordsearch.java
+++ app/src/main/java/com/puzzle/andrew/sudowordsandroid/Wordsearch/Wordsearch.java
@@ -1,14 +1,17 @@
 package com.puzzle.andrew.sudowordsandroid.wordsearch;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.puzzle.andrew.sudowordsandroid.R;
 
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by Andrew on 30/08/2017.
@@ -19,6 +22,8 @@ public class Wordsearch extends AppCompatActivity{
     Context context = Wordsearch.this;
     String dictionary1 = "words_cambridge.txt";
     String dictionary2 = "words_cambridge.txt";
+    Random rand = new Random();
+    String randomFill = "AAAAAAAAABBCCDDDDEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
     String[][] grid = new String [9][9];
     int x = 8, y = 8;
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class Wordsearch extends AppCompatActivity{
         setContentView(R.layout.wordsearch_menu);
 
         try {
-            WordSearchGenerator wordsearch = new WordSearchGenerator(context, 6, 1, dictionary1, dictionary2, "English", "English", "None");
+            WordSearchGenerator wordsearch = new WordSearchGenerator(context, 6, 2, dictionary1, dictionary2, "English", "English", "None");
             grid = wordsearch.grid;
         }catch(IOException ex){
 
@@ -50,10 +55,15 @@ public class Wordsearch extends AppCompatActivity{
             for (int j = 0; j < y-2; j++){
                 EditText field = (EditText) wordsearchGrid.getChildAt(j * 6 + i);
                 field.setBackgroundResource(R.drawable.border_active);
-                if(!grid[i][j].equals("")) {
-                    field.setText("" + grid[i+1][j+1]);
+                field.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                if(!grid[i+1][j+1].equals("_")) {
+                    field.setText("" + grid[i+1][j+1].toUpperCase());
                     field.setBackgroundResource(R.drawable.border);
                     field.setKeyListener(null);
+                }else{
+                    field.setText(Character.toString(randomFill.charAt(rand.nextInt(randomFill.length()))));
+                    //field.setBackgroundColor(Color.LTGRAY);
+                    field.setBackgroundResource(R.drawable.border2);
                 }
             }
         }
