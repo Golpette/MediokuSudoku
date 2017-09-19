81adff8779a7616f80b8f6d68ee81e1bd3e1b291
AndrewRigg <AndrewJRigg@gmail.com>
2017-09-19 01:38:59 +0100

    Took out instantiation lines

--- app/src/main/java/com/puzzle/andrew/sudowordsandroid/sudoku/GameState.java
+++ app/src/main/java/com/puzzle/andrew/sudowordsandroid/sudoku/GameState.java
@@ -12,9 +12,6 @@ public class GameState {
     int [][] end_game;
 
     public GameState(int [][] start_game, int [][] mid_game, int [][] end_game){
-     //   start_game = new int[9][9];
-      //  mid_game = new int[9][9];
-      //  end_game = new int[9][9];
         this.start_game = start_game;
         this.mid_game = mid_game;
         this.end_game = end_game;
